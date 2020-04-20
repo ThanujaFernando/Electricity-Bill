5bbe493b5fb673aacb443a7f66f2a69f3ffc4b27
Thanuja Madushan <thanujafernando36@gmail.com>
2020-04-20 16:32:11 +0530

    basic funcs

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+
+})
+export class Tab1Page {
+  calcInterval: any;
+  plugged = false;
+  cost = 0;
+  constructor() { }
+  onClickMe(Watt) {
+    if (this.plugged) {
+      this.calcInterval = setInterval(() => {
+        this.cost = this.cost + parseInt(Watt) * 150;
+        console.log("awa")
+        console.log(this.cost);
+        if (!this.plugged) {
+          clearInterval(this.calcInterval);
+        }
+      }, 3000)
+    }
+
+  }
+
+  
+  setTrue() {
+    this.plugged = !this.plugged;
+  }
+}
